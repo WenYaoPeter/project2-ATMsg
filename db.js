a95d9d375b3b4258ae23afc755cb8c15563198d6
Lee Wen Yao <peterleewenyao@gmail.com>
2018-08-20 13:36:30 +0800

    dbjs

--- db.js
+++ db.js
@@ -1,10 +1,30 @@
 const pg = require('pg');
-const config = {
-	user: 'wenyaolee',
-	host: '127.0.0.1',
-	database: 'ATMsg',
-	port: 5432
-};
+const url = require('url');
+
+if (process.env.DATABASE_URL) {
+
+	const params = url.parse(process.env.DATABASE_URL);
+	const auth = params.auth.split(':');
+
+	const config = {
+		user: auth[0],
+		password: auth[1],
+		host: params.hostname,
+		port: params.port,
+		database: params.pathname.split('/')[1],
+		ssl: true
+	};
+
+} else {
+
+	const config = {
+		user: 'wenyaolee',
+		host: '127.0.0.1',
+		database: 'ATMsg',
+		port: 5432
+	};
+
+}
 
 const poolObj = new pg.Pool(config);
 
